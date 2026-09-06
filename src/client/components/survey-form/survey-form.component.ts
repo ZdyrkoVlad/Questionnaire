d7271423ef135bb1b58437aea99d905a2f590edd
carma51 <carma51beaf@gmail.com>
2026-09-07 00:16:03 +0300

    update button

--- src/client/components/survey-form/survey-form.component.ts
+++ src/client/components/survey-form/survey-form.component.ts
@@ -201,7 +201,7 @@ export class SurveyFormComponent extends BaseComponent {
             <svg class="w-4 h-4 text-lime-900" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M5 13l4 4L19 7"/>
             </svg>
-            Зберегти оцінку
+            Save score
           </button>
 
           <!-- Next button -->
